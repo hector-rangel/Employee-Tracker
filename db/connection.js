7dcbe600bb198725db5549a02895538a5c430b37
Hector Rangel <hector.rangeljr@yahoo.com>
2021-06-11 20:33:01 -0500

    added main files

---
+++ db/connection.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql2');
+
+// connecto database
+const connection = mysql.createConnection(
+    {
+        // localhost
+        host: 'localhost',
+        //mySQL username
+        user: 'root',
+        //mySQL password
+        password: 'Ahri234#',
+        // this databse might need to be changed not sure what to name it.
+        database: 'tracker_db'
+    },
+    console.log(`Connected to the db database.`)
+);
+
+// connection (NOTE DONE YET)
+connection.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+});
+
+module.exports = connection;
---
+++ index.js
@@ -0,0 +1,16 @@
+const inquirer = require('inquirer');
+const consoleTable = require('console.table');
+
+// // start question
+// function initialize() {
+//     inquirer.prompt
+//     ([
+//         {
+//             type: 'list',
+//             name: 'choice',
+//             message: 'What do you want to do?',
+//             choices: ['View All Deparments', 'View All Roles', 'View all Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role']
+//         }
+//     ])
+// };
+
